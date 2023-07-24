df5545968e88df77e93c31cf63a40a01f36b21fd
Thanhha58 <halt@oucru.org>
2023-07-24 12:03:51 +0700

    R cars package

--- ggplot2 code.R
+++ ggplot2 code.R
@@ -2,3 +2,13 @@ library(ggplot2)
 
 ggplot(mpg, aes(displ, hwy, colour = class)) + 
   geom_point()
+
+
+# create data
+xValue <- 1:10
+yValue <- cumsum(rnorm(10))
+data <- data.frame(xValue,yValue)
+
+# Plot
+ggplot(data, aes(x=xValue, y=yValue)) +
+  geom_line()
